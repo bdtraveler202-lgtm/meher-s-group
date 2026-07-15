136198b13784bb2eade96fc94d92cc62d8db2918
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-15 17:20:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -49,3 +49,28 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 5000);
+// Hero Slider
+
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>slide.classList.remove("active"));
+
+    slides[index].classList.add("active");
+
+}
+
+setInterval(()=>{
+
+    current++;
+
+    if(current>=slides.length){
+        current=0;
+    }
+
+    showSlide(current);
+
+},4000);
