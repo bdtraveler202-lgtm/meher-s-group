8c708073e1b306ac2888fe314deebf9988025af9
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-15 18:12:28 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,25 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const name = form.querySelector('input[type="text"]').value;
+    const price = form.querySelector('input[type="number"]').value;
+    const image = form.querySelectorAll('input[type="text"]')[1].value;
+
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+
+    products.push({
+        name: name,
+        price: price,
+        image: image
+    });
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    alert("Product Added Successfully!");
+
+    form.reset();
+
+});
